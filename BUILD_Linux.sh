da11dcb0026f9bc3f7fe5c763415570a89d87497
HidekiHrk <hidekihiroki123@gmail.com>
2020-03-15 18:01:48 -0300

    .

--- BUILD_Linux.sh
+++ BUILD_Linux.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 pyinstaller -F ./main.py -i './img/icon.png'
+rm -r ./build
+cp -r ./img ./dist
+chmod +x ./dist/main
+mv ./dist/main ./dist/DRichGen
+mv ./dist './DiscordRichGenerator'
